c5c1ed2c32aaad5cebf668848c6aeb5c35693af0
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-09-11 09:48:56 -0300

    Add routes and retries

--- ssheditor/connector.py
+++ ssheditor/connector.py
@@ -1,15 +1,24 @@
 from paramiko import SSHClient, AutoAddPolicy
 class Connection:
-    def __init__(self, host="sshserver", port=22):
+    def __init__(self, hostname="sshserver", username="root", password="root"):
+        self.hostname = hostname
+        self.username = username
+        self.password = password
         self.client = SSHClient()
         self.client.set_missing_host_key_policy(AutoAddPolicy)
-        self.client.connect(hostname = host, username = "root", password = "root")
+        self.client.connect(hostname = hostname, username = username, password = password)
         self.bashIn, self.bashOut, self.bashErr = self.client.exec_command("bash")
-    def get_file_structure(self, path):
+    def get_file_structure(self, path, reconnects = 2):
         cin, cout, cerr = self.client.exec_command("ls -la " + path)
         output = cout.read().decode('ascii')
         lines = output.split('\n')
         ret = {}
+        if len(lines) < 2:
+            if reconnects > 0:
+                self.client.connect(hostname = self.hostname, username = self.username, password = self.password)
+                return self.get_file_structure(path, reconnects - 1)
+            else:
+                return "ERROR"
         lnt = len(lines[1]) -1
         for line in lines[3:]:
             if line != '':
--- ssheditor/server.py
+++ ssheditor/server.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from connector import Connection
-x = Connection()
+import sys, random, json
 
+dic = {}
 app = Flask(__name__)
-@app.route('/', methods=['GET'])
-def get_test():
-    return ''' 
-<html>
-    <head></head>
-    <body>
-    %s
-    </body>
-</html>
-''' % x.get_file_structure('/test_dir')
+@app.route('/connect/', methods=['POST'])
+def get_conn():
+    rnd = str(random.randint(0, 1000000))
+    params = request.json
+    dic[rnd] = Connection(hostname=params['hostname'], username=params['username'], password=params['password'])
+    return rnd
+
+
+@app.route('/getpath/', methods=['POST'])
+def get_path():
+    params = request.json
+    files = dic[params['key']].get_file_structure(path=params['path'])
+    return json.dumps(files)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
